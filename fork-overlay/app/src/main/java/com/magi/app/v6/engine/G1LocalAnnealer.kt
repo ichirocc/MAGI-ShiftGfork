4a901e686778687ffceea0da0ca73a18ddfbb934
ichirocc <ichiro.kosen@gmail.com>
2026-08-04 15:29:46 +0900

    fix: extension imports G1LocalAnnealer.kt

--- fork-overlay/app/src/main/java/com/magi/app/v6/engine/G1LocalAnnealer.kt
+++ fork-overlay/app/src/main/java/com/magi/app/v6/engine/G1LocalAnnealer.kt
@@ -1,4 +1,7 @@
 package com.magi.app.v6.engine
+import com.magi.app.v6.dayDemand
+import com.magi.app.v6.wishLocked
+import com.magi.app.v6.allowedShiftsForStaff
 
 import com.magi.app.v6.MirrorLog
 import com.magi.app.v6.Problem
