b6b983ed0a802ff8f9327013cd25d605941b5bef
ichirocc <ichiro.kosen@gmail.com>
2026-08-09 09:20:36 +0900

    fix: UnimprovableConstraints import shiftDemand extension

--- fork-overlay/app/src/main/java/com/magi/app/v6/engine/UnimprovableConstraints.kt
+++ fork-overlay/app/src/main/java/com/magi/app/v6/engine/UnimprovableConstraints.kt
@@ -1,6 +1,8 @@
 package com.magi.app.v6.engine
 
 import com.magi.app.v6.Problem
+import com.magi.app.v6.shiftDemand
+import com.magi.app.v6.dayDemand
 import com.magi.app.v6.constraintConfig
 import com.magi.app.v6.ViolationReport
 
